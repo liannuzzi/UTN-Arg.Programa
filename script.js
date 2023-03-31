572d6b6016646ff1fda135968b470f723d4552ca
Lucas <iannuzzilucas@hotmail.com>
2023-03-30 21:39:46 -0300

    Segundo commit

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log('hola desde archivo js')
+
+const titulo=document.getElementById('titulo')
+const boton=document.getElementById('boton')
+
+
+boton.onclick=()=>{
+    titulo.textContent="Hiciste click"
+}
