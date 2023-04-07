28e7a4fadd1e001c3502b8d9fc7b8f6135b0f6d9
Lucas <iannuzzilucas@hotmail.com>
2023-04-07 16:51:19 -0300

    Update HTML y CSS

--- Carpeta 1/script.js
+++ Carpeta 1/script.js
@@ -1,9 +1,1 @@
 console.log('hola desde archivo js')
-
-const titulo=document.getElementById('titulo')
-const boton=document.getElementById('boton')
-
-
-boton.onclick=()=>{
-    titulo.textContent="Hiciste click"
-}   
