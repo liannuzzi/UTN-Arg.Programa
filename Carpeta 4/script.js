36c9597c9c5a952abec1bf77f333dfc20652fe7a
Lucas <iannuzzilucas@hotmail.com>
2023-04-08 17:45:34 -0300

    Modificaciones Contactanos

--- Carpeta 4/script.js
+++ Carpeta 4/script.js
@@ -1,9 +1,67 @@
-console.log('hola desde archivo js')
+$(document).ready(function() {
+  $('#btnEnviarForm').click(function() {
+    event.preventDefault()
+  });
 
-const titulo=document.getElementById('titulo')
-const boton=document.getElementById('boton')
+  $('#formContactenos').validate({
+    rules:{
+        name:{
+            required:true,
+            minlength:3
+        },
+        telefono:{
+            required:true,
+            minlength:8
+        },
+        email:{
+            required:true,
+            email:true
+        },
+        consulta:{
+            required:true,
+            minlength:20
+        }
+    },
+    messages:{
+        name:{
+            required:'Por favor, ingrese su nombre y apellido',
+            minlength:"Por favor, ingrese una palabra con más de 2 caracteres"
+        },
+        telefono:{
+            required:"Por favor, ingrese su numero de telofono",
+            minlength:"La cantidad de digitos minima es de 8"
+        },
+        email:{
+            required:"Por favor, ingrese su email",
+            email:"Por favor ingrese un email valido"
+        },
+        consulta:{
+            required:"Por favor, ingrese su consulta o reclamo",
+            minlength:"La cantidad minima de caracteres es de 20"
+        }
+        
+    },
+    submitHandler:function(form){
+        form.submit()
+    }
+  })
 
+  $('#formContactenos').on('keyup change', function() {
+    if ($('#formContactenos').valid()) {
+      $('#btnEnviarForm').prop('disabled', false);
+    } else {
+      $('#btnEnviarForm').prop('disabled', true);
+    }
+  });
 
-boton.onclick=()=>{
-    titulo.textContent="Hiciste click"
-}   
+  $('#btnCerrar').click(()=>{
+    window.location.href="../Carpeta 1/index.html"
+  })
+
+  $('#btnGenerarPdf').click(()=>{
+
+  $('#formContactenos').printThis()
+
+})
+
+});
