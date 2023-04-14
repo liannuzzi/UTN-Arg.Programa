a7748253d5efa2c2db67fd85f5b433c65e6bed7a
Lucas <iannuzzilucas@hotmail.com>
2023-04-14 13:13:18 -0300

    Merge de carpetas

--- Carpeta 3/script.js
+++
@@ -1,9 +0,0 @@
-console.log('hola desde archivo js')
-
-const titulo=document.getElementById('titulo')
-const boton=document.getElementById('boton')
-
-
-boton.onclick=()=>{
-    titulo.textContent="Hiciste click"
-}   
